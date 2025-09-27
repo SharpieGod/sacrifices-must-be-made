a7fe5bf6490d8738ba06a8be125364d3cf0f098b
SharpieGod <sabodarko09@gmail.com>
2025-09-27 15:50:56 -0700

    player change

--- Assets/C#/Door.cs
+++ Assets/C#/Door.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     public Animator animator;
+    public  SceneManager sceneManager;
 
     private void Start()    
     {
@@ -11,6 +13,22 @@ public class Door : MonoBehaviour
 
     public void Exit()
     {
-        animator.SetTrigger("exit");
+        StartCoroutine(ExitRoutine());
     }
+
+    private IEnumerator ExitRoutine()
+    {
+        animator.SetTrigger("exit");
+
+        // Wait until the exit animation starts
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Exit"));
+
+        // Then wait for the animation to finish
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Now switch scenes
+        sceneManager.GoToMenu();
+    }
+
+
 }
