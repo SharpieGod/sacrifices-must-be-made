83327bf1f97bfe59c43a84d475d30d40e2787ae5
SharpieGod <sabodarko09@gmail.com>
2025-09-27 16:11:13 -0700

    ya'

--- Assets/C#/Door.cs
+++ Assets/C#/Door.cs
@@ -13,22 +13,7 @@ public class Door : MonoBehaviour
 
     public void Exit()
     {
-        StartCoroutine(ExitRoutine());
-    }
-
-    private IEnumerator ExitRoutine()
-    {
-        animator.SetTrigger("exit");
-
-        // Wait until the exit animation starts
-        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Exit"));
-
-        // Then wait for the animation to finish
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-
-        // Now switch scenes
         sceneManager.GoToMenu();
     }
-
 
 }
